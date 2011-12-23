d2e7ec8495881375d784759a5c4e4e7720752b83
Jakob Flierl <jakob.flierl@gmail.com>
2011-12-23 20:10:11 +0100

    error => warning.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 
 if [ "$USER" = "root" ]; then
-	echo "*** You cannot do this as "$USER" please use a normal user account."
-	exit 1
+	echo "*** Warning. You cannot do this as "$USER" please use a normal user account."
 fi
 if test ! -f configure.ac ; then
 	echo "*** Please invoke this script from directory containing configure.ac."
